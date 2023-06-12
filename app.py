cfc5e36fa0cd4d5b3c5147d75119c9258676077b
Mihai <95710271+mihaitza2000@users.noreply.github.com>
2023-06-12 12:59:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,13 @@
-from flask import Flask
-import requests
-import json
+from flask import Flask, render_template, jsonify, request
+import poe
 
 app = Flask(__name__)
+token = "RsnjIpIFVRX-2Q352LZGgQ%3D%3D"
+client = poe.Client(token)
 
-url = 'https://api.pawan.krd/v1/chat/completions'
-API_KEY = "pk-nqnkfaVtEKcwMTIMxjSgpWfVnVGqKKEqlLQwhgGuJGhSJrke"
-headers = {
-    'Authorization': f'Bearer {API_KEY}',
-    'Content-Type': 'application/json'
-}
-
-@app.route('/')
-def hello_world():
-    question = "how to increase my protein in my diate"
-    payload = {
-        "model": "gpt-3.5-turbo",
-        "max_tokens": 100,
-        "messages": [
-            {
-                "role": "assistant",
-                "content": question
-            },
-        ]
-    }
-    response = requests.post(url, headers=headers, data=json.dumps(payload))
-    return response.text
+@app.route('/', methods = ["GET", "POST"])
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
